45cf323fd904f9163d0e4e42b9e3eb17211da482
syeditrath <syeditrath1@gmail.com>
2026-04-02 10:57:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -325,7 +325,7 @@ function WelcomeScreen({onEnter}) {
           border:"3px solid rgba(251,191,36,0.6)",
         }}>
           <img src="logo.png" alt="Scorpion Arabia"
-            style={{width:"100%",height:"100%",objectFit:"cover",mixBlendMode:"normal"}}/>
+            style={{width:"100%",height:"100%",objectFit:"cover",display: "block"}}/>
         </div>
       </div>
 
