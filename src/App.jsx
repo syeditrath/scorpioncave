c2f782e9f28aad934bf10044404daf9d314f95ca
syeditrath <syeditrath1@gmail.com>
2026-04-06 12:56:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "";
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "";
 
-const supabase =
-  supabaseUrl && supabaseAnonKey
-    ? createClient(supabaseUrl, supabaseAnonKey)
-    : null;
 
 const supabase =
   supabaseUrl && supabaseAnonKey
